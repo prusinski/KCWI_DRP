1e88265727a382491b571b5d84cb9c9bf7114738
Max Brodheim <max.brodheim@gmail.com>
2023-08-08 11:40:49 -1000

    Force ccdddata.[data,unc] 32->64
    Quick fix to force files to be written as float32's, not float64's

--- kcwidrp/primitives/kcwi_file_primitives.py
+++ kcwidrp/primitives/kcwi_file_primitives.py
@@ -874,6 +874,7 @@ def kcwi_fits_writer(ccddata, table=None, output_file=None, output_dir=None,
     # (the .array property), make it 32 bits.
     if ccddata.uncertainty is not None and ccddata.uncertainty.array.dtype == np.float64:
         ccddata.uncertainty.array = ccddata.uncertainty.array.astype(np.float32)
+<<<<<<< HEAD
 
     if ccddata.data and ccddata.data.dtype == np.float64:
         ccddata.data = ccddata.data.astype(np.float32)
@@ -887,6 +888,15 @@ def kcwi_fits_writer(ccddata, table=None, output_file=None, output_dir=None,
     if ccddata.uncertainty is not None and ccddata.uncertainty.array.dtype == np.float64:
         ccddata.uncertainty.array = ccddata.uncertainty.array.astype(np.float32)
 
+=======
+    
+    if ccddata.data and ccddata.data.dtype == np.float64:
+        ccddata.data = ccddata.data.astype(np.float32)
+
+    if ccddata.uncertainty and ccddata.uncertainty.dtype == np.float64:
+        ccddata.uncertainty = ccddata.uncertainty.astype(np.float32)
+    
+>>>>>>> f29afec (Force ccdddata.[data,unc] 32->64)
     out_file = os.path.join(output_dir, os.path.basename(output_file))
     if suffix is not None:
         (main_name, extension) = os.path.splitext(out_file)
